f0111f5f58e19c14c1487dbb60925fe25354c7fd
casini <bian_casini@mi.unc.edu.ar>
2026-04-28 17:22:17 -0300

    feat: implementado el ejercicio 3 en Analyzer.scala y NamedEntity.scala.  fix: sacar main de testeo de Dictionary.scala

--- src/main/scala/NamedEntity.scala
+++ src/main/scala/NamedEntity.scala
@@ -2,6 +2,14 @@
 abstract class NamedEntity(val text: String) {
   def entityType: String
   def describe: String = s"[$entityType] $text"
+  
+  // ejercicio 3: este metodo sirve para que cada entidad sepa decir si ella misma aparece en un texto
+  // esto es encapsular: el mismo objeto es el responsable de encontrarse en el post
+  def isPresentIn(content: String): Boolean = {
+  // comparar con el texto en minusculas para que la búsqueda sea case insensitive
+  // isPresentIn recibe el contenido en minuscula desde afuera por eficiencia (para no hacerlo 1000 veces aca)
+    content.contains(this.text.toLowerCase)  // "this" es la entidad
+    }
 }
 
 class Person(text: String) extends NamedEntity(text) {
@@ -27,3 +35,4 @@ class University(text: String) extends Organization(text) {
 class ProgrammingLanguage(text: String) extends Technology(text) {
   override def entityType: String = "ProgrammingLanguage"
 }
+
